71dab8095e077402b905cd28e330e821e1a68429
Alice Zhang <anz0063@gmail.com>
2022-04-01 20:11:16 -0400

    bot can reply

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import DiscordJS, { Intents, Message } from 'discord.js'
 import dotenv from 'dotenv'
+
+
 dotenv.config()
 
 const client = new DiscordJS.Client({
@@ -16,7 +18,10 @@ client.on('ready', () => {
 client.on('messageCreate', (message) => {
     if(message.content === '$concerndle'){
         //will create this function later
-        message.startGame();
+        message.reply({
+            content: 'wassup',
+        })
+        //message.startGame();
     }
 })
 
