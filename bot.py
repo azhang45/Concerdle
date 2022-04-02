09a3df3bf9e05e23a09668e9c9f380bea24b50ab
Alice Zhang <anz0063@gmail.com>
2022-04-02 11:08:50 -0400

    seperated methods to new file

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import nextcord
 from nextcord.ext import commands
 import config
+import methods
 import linecache
 import random
 
@@ -9,23 +10,6 @@ PREFIX = "$"
 SOLVED = True
 correctWord = ""
 
-def chooseWord(filename):
-    return linecache.getline(filename, random.randint(0, 2314))
-
-def checkCorrect(guess, correct):
-    white = '<:white_large_square:959676720930258984>'
-    yellow = "<:yellow_square:959677336750534686>"
-    green = "<:green_square:959677336750534686>"
-    ans = ""
-
-    for i in range(5):
-        if guess[i] not in correct:
-            ans += white
-        elif guess[i] in correct and correct[i] != guess[i]:
-            ans += yellow
-        else:
-            ans += green
-    return ans
 
 @client.event
 async def on_ready():
@@ -45,11 +29,11 @@ async def on_message(message):
         global correctWord
         if message.content[1:] == 'concerndle' and SOLVED:
             SOLVED = False
-            correctWord = chooseWord("wordle-answers-alphabetical.txt")
+            correctWord = methods.chooseWord("wordle-answers-alphabetical.txt")
             await message.channel.send(correctWord)
             return
         elif len(message.content) == 6 and not SOLVED:
-            await message.channel.send(checkCorrect(message.content[1:], correctWord))
+            await message.channel.send(methods.checkCorrect(message.content[1:], correctWord))
             return
             
 
---
+++ methods.py
@@ -0,0 +1,20 @@
+import random
+import linecache
+
+def chooseWord(filename):
+    return linecache.getline(filename, random.randint(0, 2314))
+
+def checkCorrect(guess, correct):
+    white = '<:white_large_square:959676720930258984>'
+    yellow = "<:yellow_square:959677336750534686>"
+    green = "<:green_square:959677336750534686>"
+    ans = ""
+
+    for i in range(5):
+        if guess[i] not in correct:
+            ans += white
+        elif guess[i] in correct and correct[i] != guess[i]:
+            ans += yellow
+        else:
+            ans += green
+    return ans
